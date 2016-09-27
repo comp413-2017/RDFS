9b0a6cde7592dc080cd2d07b7ecae88d18864799
Peter Elmers <peter.elmers@yahoo.com>
2016-09-27 17:16:06 -0400

    Read handshake, payload size

--- rice-namenode/main.cc
+++ rice-namenode/main.cc
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <netinet/in.h>
 #include <thread>
 #include <iostream>
 #include <asio.hpp>
@@ -10,25 +11,50 @@ static bool receive_handshake(tcp::socket& sock, short* version, short* service,
     // Return true on success, false on failure.
     // If we succeed, version, service, and auth protocol set from the received
     // handshake header.
+    // Handshake has 7 bytes.
+    constexpr size_t handshake_len = 7;
+    asio::error_code error;
+    tcp::socket::message_flags flags;
+    char data[handshake_len];
+    size_t rec_len = sock.receive(asio::buffer(data, handshake_len), flags, error);
+    if (!error && rec_len == handshake_len) {
+        // First 4 bytes are 'hrpc'
+        if (data[0] == 'h' && data[1] == 'r' && data[2] == 'p' && data[3] == 'c') {
+            *version = data[4];
+            *service = data[5];
+            *auth_protocol = data[6];
+            return true;
+        }
+    }
     return false;
 }
 
-// TODO: mkdir
-static void session(tcp::socket sock) {
-    constexpr size_t buflen = 1024;
+static bool readint32(tcp::socket& sock, uint32_t* out) {
+    uint32_t data;
     asio::error_code error;
     tcp::socket::message_flags flags;
-    std::uint32_t size;
-    size_t length = sock.receive(asio::buffer(&size, sizeof(size)), flags, error);
-    std::cout << size << std::endl;
-    for (;;) {
-        char data[buflen];
-        size_t length = sock.receive(asio::buffer(data, buflen), flags, error);
-        if (error) {
-            std::cout << "Something unexpected." << std::endl;
-            break;
-        }
-        std::cout << length << std::endl;
+    size_t rec_len = sock.receive(asio::buffer(&data, 4), flags, error);
+    if (!error && rec_len == 4) {
+        *out = ntohl(data);
+        return true;
+    }
+    return false;
+}
+
+static void handle_rpc(tcp::socket sock) {
+    // Remark: No need to close socket, it happens automatically in its
+    // destructor.
+    short version, service, auth_protocol;
+    if (receive_handshake(sock, &version, &service, &auth_protocol)) {
+        std::printf("Got handshake: version=%d, service=%d, protocol=%d\n", version, service, auth_protocol);
+    } else {
+        std::cout << "Failed to receive handshake." << std::endl;
+    }
+    uint32_t payload_len;
+    if (readint32(sock, &payload_len)) {
+        std::printf("Got payload length: %d\n", payload_len);
+    } else {
+        std::cout << "Failed to receive payload length." << std::endl;
     }
 }
 
@@ -45,6 +71,6 @@ int main(int argc, char* argv[]) {
     for (;;) {
         tcp::socket sock(io_service);
         a.accept(sock);
-        std::thread(session, std::move(sock)).detach();
+        std::thread(handle_rpc, std::move(sock)).detach();
     }
 }
