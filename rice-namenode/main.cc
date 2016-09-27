d1cc9f044200fe135f739e7bb5f7ab1eefadf2dc
Peter Elmers <peter.elmers@yahoo.com>
2016-09-27 16:05:52 -0400

    WIP RPC Server

--- rice-namenode/main.cc
+++ rice-namenode/main.cc
@@ -1,7 +1,50 @@
-#include <cstdio>
+#include <cstdlib>
+#include <thread>
+#include <iostream>
+#include <asio.hpp>
+#include <RpcHeader.pb.h>
 
-using namespace std;
+using asio::ip::tcp;
 
-int main() {
-    printf("I am NameNode.\n");
+static bool receive_handshake(tcp::socket& sock, short* version, short* service, short* auth_protocol) {
+    // Return true on success, false on failure.
+    // If we succeed, version, service, and auth protocol set from the received
+    // handshake header.
+    return false;
+}
+
+// TODO: mkdir
+static void session(tcp::socket sock) {
+    constexpr size_t buflen = 1024;
+    asio::error_code error;
+    tcp::socket::message_flags flags;
+    std::uint32_t size;
+    size_t length = sock.receive(asio::buffer(&size, sizeof(size)), flags, error);
+    std::cout << size << std::endl;
+    for (;;) {
+        char data[buflen];
+        size_t length = sock.receive(asio::buffer(data, buflen), flags, error);
+        if (error) {
+            std::cout << "Something unexpected." << std::endl;
+            break;
+        }
+        std::cout << length << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    asio::io_service io_service;
+    short port = 5351;
+    if (argc == 2) {
+        port = std::atoi(argv[1]);
+    }
+    std::cout << "Listen on :" << port << std::endl;
+
+    tcp::acceptor a(io_service, tcp::endpoint(tcp::v4(), port));
+
+    for (;;) {
+        tcp::socket sock(io_service);
+        a.accept(sock);
+        std::thread(session, std::move(sock)).detach();
+    }
 }
