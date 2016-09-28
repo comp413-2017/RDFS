d605f3d693cf4832cf6bc5867eae006f45a7ab36
Peter Elmers <peter.elmers@yahoo.com>
2016-09-28 16:05:23 -0400

    Refactor server into own namespace, begin listen loop.

--- rice-namenode/main.cc
+++ rice-namenode/main.cc
@@ -9,87 +9,128 @@
 
 using asio::ip::tcp;
 
+#define ERROR_AND_RETURN(msg) ::std::cerr << msg << ::std::endl; return
 #define ERROR_AND_FALSE(msg) ::std::cerr << msg << ::std::endl; return false
 
-/**
- * Return true on success, false on failure. On success, version, service,
- * and auth protocol set from the received
- */
-static bool receive_handshake(tcp::socket& sock, short* version, short* service, short* auth_protocol) {
-    // handshake header.
-    // Handshake has 7 bytes.
-    constexpr size_t handshake_len = 7;
-    asio::error_code error;
-    char data[handshake_len];
-    size_t rec_len = sock.read_some(asio::buffer(data, handshake_len), error);
-    if (!error && rec_len == handshake_len) {
-        // First 4 bytes are 'hrpc'
-        if (data[0] == 'h' && data[1] == 'r' && data[2] == 'p' && data[3] == 'c') {
-            *version = data[4];
-            *service = data[5];
-            *auth_protocol = data[6];
-            return true;
+namespace rpcserver {
+    /**
+     * Return true on success, false on failure. On success, version, service,
+     * and auth protocol set from the received
+     */
+    bool receive_handshake(tcp::socket& sock, short* version, short* service, short* auth_protocol) {
+        // handshake header.
+        // Handshake has 7 bytes.
+        constexpr size_t handshake_len = 7;
+        asio::error_code error;
+        char data[handshake_len];
+        size_t rec_len = sock.read_some(asio::buffer(data, handshake_len), error);
+        if (!error && rec_len == handshake_len) {
+            // First 4 bytes are 'hrpc'
+            if (data[0] == 'h' && data[1] == 'r' && data[2] == 'p' && data[3] == 'c') {
+                *version = data[4];
+                *service = data[5];
+                *auth_protocol = data[6];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Attempt to receive the RPC prelude from the socket.
+     * Return whether the attempt was succesful.
+     */
+    bool receive_prelude(tcp::socket& sock) {
+        // Attempt to receive the RPC prelude (header + context) from the socket.
+        asio::error_code error;
+        uint32_t prelude_len;
+        if (read_int32(sock, &prelude_len)) {
+            std::cout << "Got prelude length: " << prelude_len << std::endl;
+        } else {
+            ERROR_AND_FALSE("Failed to receive message prelude length.");
+        }
+        char* prelude = new char[prelude_len];
+        uint64_t header_len;
+        sock.read_some(asio::buffer(prelude, prelude_len), error);
+        if (error) {
+            ERROR_AND_FALSE("Failed to read message prelude.");
+        }
+        uint64_t offset;
+        hadoop::common::RpcRequestHeaderProto rpc_request_header;
+        if (read_proto(prelude, prelude_len, rpc_request_header, &offset)) {
+            std::cout << rpc_request_header.DebugString() << std::endl;
+        } else {
+            ERROR_AND_FALSE("Couldn't read the request header");
+        }
+        // Now read the length of the IpcConnectionContext and deserialize.
+        hadoop::common::IpcConnectionContextProto connection_context;
+        if (read_proto(prelude + offset, prelude_len - offset, connection_context)) {
+            std::cout << connection_context.DebugString() << std::endl;
+        } else {
+            ERROR_AND_FALSE("Couldn't read the ipc connection context");
+        }
+        delete[] prelude;
+        return true;
+    }
+
+    /**
+     * Handle a single RPC connection.
+     */
+    void handle_rpc(tcp::socket sock) {
+        // Remark: No need to close socket, it happens automatically in its
+        // destructor.
+        asio::error_code error;
+        short version, service, auth_protocol;
+        if (receive_handshake(sock, &version, &service, &auth_protocol)) {
+            std::cout << "Got handshake version=" << version << ", service=" << service
+                      << " protocol=" << auth_protocol << std::endl;
+        } else {
+            ERROR_AND_RETURN("Failed to receive handshake.");
+        }
+        if (receive_prelude(sock)) {
+            std::cout << "Received whole prelude." << std::endl;
+        } else {
+            ERROR_AND_RETURN("Failed to receive prelude.");
+        }
+        for (;;) {
+            // Main listen loop for RPC commands.
+            uint32_t payload_size;
+            if (!read_int32(sock, &payload_size)) {
+                ERROR_AND_RETURN("Failed to payload size.");
+            }
+            std::cout << "Got payload size: " << payload_size << std::endl;
+            char* payload = new char[payload_size];
+            sock.read_some(asio::buffer(payload, payload_size), error);
+            if (error) {
+                ERROR_AND_RETURN("Failed to read payload.");
+            }
+            uint64_t offset;
+            hadoop::common::RpcRequestHeaderProto rpc_request_header;
+            if (read_proto(payload, payload_size, rpc_request_header, &offset)) {
+                std::cout << rpc_request_header.DebugString() << std::endl;
+            } else {
+                ERROR_AND_RETURN("Failed to read the request header");
+            }
+            uint64_t rpc_version;
+            if (payload_size - offset > sizeof(uint64_t)) {
+                rpc_version = (uint64_t) (payload + offset);
+                offset += sizeof(uint64_t);
+                std::cout << "rpcversion=" << rpc_version << std::endl;
+            } else {
+                ERROR_AND_RETURN("Failed to read rpc version.");
+            }
+            /*
+             * rpcVersion, err := readint64(r)
+             * declaringClassProtocolName, err := readString(r)
+             * methodName, err := readString(r)
+             * clientVersion, err := readint64(r)
+             * clientMethodHash, err := readint32(r)
+             * parameterClassesLength, err := readint32(r)
+             */
+            delete[] payload;
         }
     }
-    return false;
-}
 
-/**
- * Attempt to receive the RPC prelude from the socket.
- * Return whether the attempt was succesful.
- */
-static bool receive_prelude(tcp::socket& sock) {
-    // Attempt to receive the RPC prelude (header + context) from the socket.
-    asio::error_code error;
-    uint32_t prelude_len;
-    if (read_int32(sock, &prelude_len)) {
-        std::cout << "Got prelude length: " << prelude_len << std::endl;
-    } else {
-        ERROR_AND_FALSE("Failed to receive message prelude length.");
-    }
-    char* prelude = new char[prelude_len];
-    uint64_t header_len;
-    sock.read_some(asio::buffer(prelude, prelude_len), error);
-    std::string prelude_str(prelude, prelude_len);
-    if (error) {
-        ERROR_AND_FALSE("Failed to read message prelude.");
-    }
-    uint64_t offset;
-    hadoop::common::RpcRequestHeaderProto rpc_request_header;
-    if (read_proto(prelude, prelude_len, rpc_request_header, &offset)) {
-        std::cout << rpc_request_header.DebugString() << std::endl;
-    } else {
-        ERROR_AND_FALSE("Couldn't read the request header");
-    }
-    // Now read the length of the IpcConnectionContext and deserialize.
-    hadoop::common::IpcConnectionContextProto connection_context;
-    if (read_proto(prelude + offset, prelude_len - offset, connection_context)) {
-        std::cout << connection_context.DebugString() << std::endl;
-    } else {
-        ERROR_AND_FALSE("Couldn't read the ipc connection context");
-    }
-    delete[] prelude;
-    return true;
-}
-
-/**
- * Handle a single RPC connection.
- */
-static void handle_rpc(tcp::socket sock) {
-    // Remark: No need to close socket, it happens automatically in its
-    // destructor.
-    short version, service, auth_protocol;
-    if (receive_handshake(sock, &version, &service, &auth_protocol)) {
-        std::cout << "Got handshake version= " << version << ", service=" << service
-                  << " protocol=" << auth_protocol << std::endl;
-    } else {
-        std::cout << "Failed to receive handshake." << std::endl;
-    }
-    if (receive_prelude(sock)) {
-        std::cout << "Received whole prelude." << std::endl;
-    } else {
-        std::cerr << "Error on receiving prelude." << std::endl;
-    }
 }
 
 int main(int argc, char* argv[]) {
@@ -105,6 +146,6 @@ int main(int argc, char* argv[]) {
     for (;;) {
         tcp::socket sock(io_service);
         a.accept(sock);
-        std::thread(handle_rpc, std::move(sock)).detach();
+        std::thread(rpcserver::handle_rpc, std::move(sock)).detach();
     }
 }
--- rice-namenode/socket_reads.cc
+++ rice-namenode/socket_reads.cc
@@ -4,73 +4,128 @@
 
 using asio::ip::tcp;
 
-/**
- * Attempt to read a byte from the socket. Return true on success and set *byte
- * to its value. Otherwise return false.
- */
-static bool read_byte(tcp::socket& sock, unsigned char* byte) {
-    asio::error_code error;
-    unsigned char read[1];
-    size_t rec_len = sock.read_some(asio::buffer(read, 1), error);
-    *byte = read[0];
-    return !error && rec_len == 1;
-}
+namespace rpcserver {
 
-
-/**
- * Attempt to read big-endian uint32 from provided socket. Consumes 4 bytes.
- * Return false means failure. Otherwise return true and set *out.
- */
-static bool read_int32(tcp::socket& sock, uint32_t* out) {
-    uint32_t data;
-    asio::error_code error;
-    size_t rec_len = sock.read_some(asio::buffer(&data, 4), error);
-    if (!error && rec_len == 4) {
-        *out = ntohl(data);
-        return true;
+    /**
+     * Attempt to read a byte from the socket. Return true on success and set *byte
+     * to its value. Otherwise return false.
+     */
+    bool read_byte(tcp::socket& sock, unsigned char* byte) {
+        asio::error_code error;
+        unsigned char read[1];
+        size_t rec_len = sock.read_some(asio::buffer(read, 1), error);
+        *byte = read[0];
+        return !error && rec_len == 1;
     }
-    return false;
-}
 
 
-/**
- * Given unsigned character buffer with capacity cap, attempt to read a varint.
- * Put the value of the varint in *out, and return the number of bytes consumed.
- */
-static size_t read_varint(char *buf, size_t cap, uint64_t* out) {
-    size_t idx = 0;
-    size_t shift = 0;
-    uint64_t val = 0;
-    do {
-        val |= (buf[idx] & 0x7F) << shift;
-        idx++;
-        shift += 7;
-    } while (buf[idx] & 0x80 && idx < cap);
-    *out = val;
-    return idx;
-}
-
-
-/**
- * Attempt to parse given protocol from provided buffer with capactiy limit.
- * Return whether the parse was successful. If successful, set *consumed to the
- * number of bytes consumed by the read.
- */
-static bool read_proto(char *buf, size_t cap, ::google::protobuf::Message& proto, uint64_t *consumed) {
-    uint64_t len;
-    size_t skip = read_varint(buf, cap, &len);
-    std::string proto_str(buf + skip, std::min(len, cap - skip));
-    if (consumed != NULL) {
-        *consumed = skip + len;
+    /**
+     * Attempt to read big-endian uint16 from provided socket. Consumes 2 bytes.
+     * Return false means failure. Otherwise return true and set *out.
+     */
+    bool read_int16(tcp::socket& sock, uint16_t* out) {
+        uint16_t data;
+        asio::error_code error;
+        size_t rec_len = sock.read_some(asio::buffer(&data, 2), error);
+        if (!error && rec_len == 2) {
+            *out = ntohs(data);
+            return true;
+        }
+        return false;
     }
-    return proto.ParseFromString(proto_str);
-}
 
 
-/**
- * Attempt to parse given protocol from provided buffer with capactiy limit.
- * Return whether the parse was successful.
- */
-static bool read_proto(char* buf, size_t cap, ::google::protobuf::Message& proto) {
-    return read_proto(buf, cap, proto, NULL);
+    /**
+     * Attempt to read big-endian uint32 from provided socket. Consumes 4 bytes.
+     * Return false means failure. Otherwise return true and set *out.
+     */
+    bool read_int32(tcp::socket& sock, uint32_t* out) {
+        uint32_t data;
+        asio::error_code error;
+        size_t rec_len = sock.read_some(asio::buffer(&data, 4), error);
+        if (!error && rec_len == 4) {
+            *out = ntohl(data);
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Attempt to read big-endian uint64 from provided socket. Consumes 8 bytes.
+     * Return false means failure. Otherwise return true and set *out.
+     */
+    bool read_int64(tcp::socket& sock, uint64_t* out) {
+        uint64_t data;
+        asio::error_code error;
+        size_t rec_len = sock.read_some(asio::buffer(&data, 8), error);
+        if (!error && rec_len == 8) {
+            *out = be64toh(data);
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Attempt to read a string from provided socket. On failure, return empty
+     * string.
+     */
+    std::string read_string(tcp::socket& sock) {
+        uint16_t len;
+        if (read_int16(sock, &len)) {
+            asio::error_code error;
+            char buf[len];
+            size_t rec_len = sock.read_some(asio::buffer(buf, len), error);
+            if (!error && rec_len == len) {
+                return std::string(buf, len);
+            }
+        }
+        std::string empty;
+        return empty;
+    }
+
+
+    /**
+     * Given unsigned character buffer with capacity cap, attempt to read a varint.
+     * Put the value of the varint in *out, and return the number of bytes consumed.
+     */
+    size_t read_varint(char *buf, size_t cap, uint64_t* out) {
+        size_t idx = 0;
+        size_t shift = 0;
+        uint64_t val = 0;
+        do {
+            val |= (buf[idx] & 0x7F) << shift;
+            idx++;
+            shift += 7;
+        } while (buf[idx] & 0x80 && idx < cap);
+        *out = val;
+        return idx;
+    }
+
+
+    /**
+     * Attempt to parse given protocol from provided buffer with capactiy limit.
+     * Return whether the parse was successful. If successful, set *consumed to the
+     * number of bytes consumed by the read.
+     */
+    bool read_proto(char *buf, size_t cap, ::google::protobuf::Message& proto, uint64_t *consumed) {
+        uint64_t len;
+        size_t skip = read_varint(buf, cap, &len);
+        std::string proto_str(buf + skip, std::min(len, cap - skip));
+        if (consumed != NULL) {
+            *consumed = skip + len;
+        }
+        return proto.ParseFromString(proto_str);
+    }
+
+
+    /**
+     * Attempt to parse given protocol from provided buffer with capactiy limit.
+     * Return whether the parse was successful.
+     */
+    bool read_proto(char* buf, size_t cap, ::google::protobuf::Message& proto) {
+        return read_proto(buf, cap, proto, NULL);
+    }
+
 }
