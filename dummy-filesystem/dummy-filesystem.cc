c0ca73fe4c4795c59426afec83247a504702ec43
Caroline Adams <cea4@rice.edu>
2016-10-17 00:13:32 -0500

    Added method signatures for dummy filesystem

--- dummy-filesystem/dummy-filesystem.cc
+++ dummy-filesystem/dummy-filesystem.cc
@@ -5,4 +5,11 @@
 
 std::map<int, string> blockMap;
 
+// Given an ID, allocate a block. Returns a status message
+int::allocateBlock(long id, unsigned char[] blk)
+ 
+// Given an ID, returns a block buffer
+unsigned char[]::getBlock(long id)
 
+// Given an ID, deletes a block. Returns a status message
+int::rmBlock(long id)
