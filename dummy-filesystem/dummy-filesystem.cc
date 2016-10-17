bcb15bb9aed811035819886b8391c52180f9e39d
Caroline Adams <cea4@rice.edu>
2016-10-17 16:17:42 -0500

    Wrote the getBlock function and changed hashmap of blocks from ints to longs i Please enter the commit message for your changes. Lines starting

--- dummy-filesystem/dummy-filesystem.cc
+++ dummy-filesystem/dummy-filesystem.cc
@@ -1,21 +1,59 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 
-std::map<int, string> blockMap;
+// Maps block id to filename where it's being stored
+std::map<long, string> blockMap;
 
 // Given an ID, allocate a block. Returns a status message
 int allocateBlock(long id, unsigned char[] blk)
 {
-
+	
 }
+// TODO: Potentially return value if error reading file/allocating space
+// TODO: Decide where we're storing files
 // Given an ID, returns a block buffer
 unsigned char[] getBlock(long id)
 {
+	// Look in map and get filename
+	string filename = blockMap[id];
+	
+	// Open file
+	FILE* file;
+	file = fopen(filename, "r");
+	if (file == NULL) {
+		fputs("Error opening file", stderr);
+		exit(2);
+	}
+
+	// Find file size and allocate enough space
+	long fileSize = fseek(file, 0, SEEK_END);
+	rewind(file);
+	unsigned char* blk = (unsigned char*) malloc(sizeof(unsigned char)*fileSize);
+	if (blk == NULL) {
+		fputs("Error allocating memory", stderr)
+		exit(3);
+	}
+
+	// Copy file into buffer
+	size_t bytesRead = fread(blk, 1, fileSize, file);
+	if (bytesRead != fileSize) {
+		fputs("Error reading in file", stderr);
+		exit(3);
+	}
+
+	// Close file
+	fclose(file);
+
+	// Return buffer
+	return blk;
 }
 
 // Given an ID, deletes a block. Returns a status message
 int rmBlock(long id)
 {
-}
+
+}         
