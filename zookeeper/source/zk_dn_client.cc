e8ba0cc80ee6ce4be729bf15b10a4d8c28f08ec4
vagrant <vagrant@vagrant.vm>
2016-11-04 20:11:30 +0000

    fixed merge_conflicts

--- zookeeper/source/zk_dn_client.cc
+++ zookeeper/source/zk_dn_client.cc
@@ -24,9 +24,6 @@ namespace zkclient{
 
         registerDataNode();
 		LOG(INFO) << "Registered datanode " + build_datanode_id(data_node_id);
-		// TODO remove this, it's only for demo purposes
-		blockReceived(12345);
-
 	}
 
 	bool ZkClientDn::blockReceived(uint64_t uuid) {
@@ -43,36 +40,20 @@ namespace zkclient{
 			LOG(ERROR) << CLASS_NAME <<  "Failed locking on /work_queues/wait_for_acks/<block_uuid> " << error_code;
 		}
 		if (zk->exists(WORK_QUEUES + WAIT_FOR_ACK_BACKSLASH + std::to_string(uuid), exists, error_code)) {
-<<<<<<< HEAD
 			if (exists) {
 				if(zk->create(WORK_QUEUES + WAIT_FOR_ACK_BACKSLASH + std::to_string(uuid) + "/" + id, ZKWrapper::EMPTY_VECTOR, error_code, true)) {
 					LOG(ERROR) << CLASS_NAME <<  "Failed to create wait for acks " << error_code;
 				}
 			}
 		}
-=======
-			if (!exists) {
-				if(!zk->create(WORK_QUEUES + WAIT_FOR_ACK_BACKSLASH + std::to_string(uuid), ZKWrapper::EMPTY_VECTOR, error_code)) {
-					LOG(ERROR) << CLASS_NAME <<  "Failed to create wait_for_acks/<block_uuid> " << error_code;
-				}
-			}
-			if(!zk->create(WORK_QUEUES + WAIT_FOR_ACK_BACKSLASH + std::to_string(uuid) + "/" + id, ZKWrapper::EMPTY_VECTOR, error_code, true)) {
-				LOG(ERROR) << CLASS_NAME <<  "Failed to create wait_for_acks/<block_uuid>/datanode_id " << error_code;
-			}
-		}
 
->>>>>>> zk-dn-client
 		if (queue_lock.unlock() != 0) {
 			LOG(ERROR) << CLASS_NAME <<  "Failed unlocking on /work_queues/wait_for_acks/<block_uuid> " << error_code;
 		}
 
 		// Create a block_locations node for this block if there isn't one already
 		// TODO this should always exist
-<<<<<<< HEAD
 		if (!zk->exists(BLOCK_LOCATIONS + std::to_string(uuid), exists, error_code)) {
-=======
-		if (zk->exists(BLOCK_LOCATIONS + std::to_string(uuid), exists, error_code)) {
->>>>>>> zk-dn-client
         	if (!exists) {
 				LOG(INFO) << "This block did not exist so we are creating it";
         		if(!zk->create(BLOCK_LOCATIONS + std::to_string(uuid), ZKWrapper::EMPTY_VECTOR, error_code, false)) {
