ff6ec3ba0e02c0a0114c858b538492536cb8d2ae
Prudhvi Boyapalli <prrb02@gmail.com>
2016-11-05 17:12:43 -0500

    Added docstrings to queue functions

--- zkwrapper/zk_queue.h
+++ zkwrapper/zk_queue.h
@@ -10,29 +10,40 @@
 #include "zkwrapper.h"
 
 class ZKQueue {
-    public:
-        ZKQueue(ZKWrapper &wrapper, std::string path) : zk(wrapper) {
-                q_path = path;
-                element = path + "/q_item-";
-                peek_path = "";
-                int error_code;
-                if (!zk.create(path, ZKWrapper::EMPTY_VECTOR, error_code)) {
-                    // TODO: Handle erro
-                }
-        }
+	public:
+		ZKQueue(ZKWrapper &wrapper, std::string path) : zk(wrapper) {
+				q_path = path;
+				element = path + "/q_item-";
+				peek_path = "";
+				int error_code;
+				if (!zk.create(path, ZKWrapper::EMPTY_VECTOR, error_code)) {
+					// TODO: Handle erro
+				}
+		}
 
-        std::string peek();
+		/**
+		 * Returns the name of the first item in the queue
+		 */
+		std::string peek();
 
-        int pop();
+		/**
+		 * Removes the first item from the queue.
+		 * Returns 0 if successful, otherwise an error code
+		 */
+		int pop();
 
-        std::string push(const std::vector<std::uint8_t> &data);
+		/**
+		 * Adds a new item to the end of the queue with the given data.
+		 * Returns the path of the newly added element
+		 */
+		std::string push(const std::vector<std::uint8_t> &data);
 
-    private:
-        std::string q_path;
-        std::string peek_path;
-        std::string element;
-        ZKWrapper zk;
-        static const std::string CLASS_NAME;
+	private:
+		std::string q_path;
+		std::string peek_path;
+		std::string element;
+		ZKWrapper zk;
+		static const std::string CLASS_NAME;
 };
 
 #endif //RDFS_ZK_QUEUE_H
