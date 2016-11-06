9e822851b68b2e7f2f85dc8c06fe7b29a0123b34
Allison Gardella <ajg9@rice.edu>
2016-11-06 03:05:09 +0000

    Add doctrings for zk_dn_client

--- zookeeper/include/zk_dn_client.h
+++ zookeeper/include/zk_dn_client.h
@@ -26,18 +26,46 @@ typedef struct
     char ipPort[256];
 } DataNodeZNode;
 
+/**
+* Class representing a Zookeeper-DataNode client.
+* Allows a DataNode to update the state of ZK.
+*/
 class ZkClientDn : public ZkClientCommon {
 
 public:
+	/**
+	* Initializes the Zookeeper-Datanode client.
+	* @param ip The DataNode IP
+	* @param hostname The DataNode hostname
+	* @param zkIpAndAddress The IP and hostname of ZK
+	* @param ipcPort TODO
+	* @param xferPort TODO
+	*/
     ZkClientDn(const std::string& ip, const std::string& hostname, const std::string& zkIpAndAddress,
                        const uint32_t ipcPort = 50020, const uint32_t xferPort = 50010);
     ~ZkClientDn();
+
+	/**
+	* Registers this DataNode with Zookeeper.
+	*/
     void registerDataNode();
+
+	/**
+	* Informs Zookeeper when the DataNode has received a block. Adds an acknowledgment
+	* and creates a node for the DN in the block's block_locations.
+	* @param uuid The UUID of the block received by the DataNode.
+	* @return True on success, false on error.
+	*/
     bool blockReceived(uint64_t uuid);
 
 
 private:
 
+	/**
+	* Builds a string of the DataNode ID.
+	* @param data_node_id The DataNode's DataNodeId object, containing the IP and port.
+	* @return The ID string
+	*/
     std::string build_datanode_id(DataNodeId data_node_id);
 
     DataNodeId data_node_id;
