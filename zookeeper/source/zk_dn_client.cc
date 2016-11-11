ddcbcc605555f8a64e182300e12ac34b88d9f9ce
captain695 <cea4@rice.edu>
2016-11-10 20:04:23 -0600

    Delete heartbeat if it already exists during registration

--- zookeeper/source/zk_dn_client.cc
+++ zookeeper/source/zk_dn_client.cc
@@ -129,10 +129,13 @@ namespace zkclient{
 		// Create an ephemeral node at /health/<datanode_id>/heartbeat
 		// if it doesn't already exist. Should have a ZOPERATIONTIMEOUT
 		if (zk->exists(HEALTH_BACKSLASH + id + "/heartbeat", exists, error_code)) {
-			if (!exists) {
-				if (!zk->create(HEALTH_BACKSLASH + id + "/heartbeat", ZKWrapper::EMPTY_VECTOR, error_code, true)) {
+			if (exists) {
+				if (!zk->delete_node(HEALTH_BACKSLASH + id + "/heartbeat", error_code)) {
 					// TODO: Handle error
 				}
+			}
+			if (!zk->create(HEALTH_BACKSLASH + id + "/heartbeat", ZKWrapper::EMPTY_VECTOR, error_code, true)) {
+				// TODO: Handle error
 			}
 		}
 
