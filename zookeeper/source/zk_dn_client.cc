2d4b87a4b8c6e84df17d54820cc998bcd7d46f8a
Allison Gardella <ajg9@rice.edu>
2016-11-11 17:21:20 +0000

    Fix addition of /blocks under /health

--- zookeeper/source/zk_dn_client.cc
+++ zookeeper/source/zk_dn_client.cc
@@ -82,7 +82,7 @@ namespace zkclient{
 			created_correctly = false;
 		}
 
-		LOG(INFO) << "About to create the znode for the data ip";
+
 		if (zk->exists(BLOCK_LOCATIONS + std::to_string(uuid), exists, error_code)) {
 			if (exists) {
 				// Write the block size
@@ -107,6 +107,16 @@ namespace zkclient{
 			}
 		}
 
+		// Write block to /blocks
+		if (zk->exists(HEALTH_BACKSLASH + id + BLOCKS, exists, error_code)) {
+			if (exists) {
+				if (!zk->create(HEALTH_BACKSLASH + id + BLOCKS + "/" + std::to_string(uuid), ZKWrapper::EMPTY_VECTOR, error_code)) {
+					LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/blocks/<block_uuid> " << error_code;
+				}
+			}
+		}
+
+
 		return created_correctly;
 	}
 
@@ -120,29 +130,22 @@ namespace zkclient{
 		if (zk->exists(HEALTH_BACKSLASH + id, exists, error_code)) {
 			if (!exists) {
 				if (!zk->create(HEALTH_BACKSLASH + id, ZKWrapper::EMPTY_VECTOR, error_code)) {
-					LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/blocks" << error_code;
+					LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id> " << error_code;
 				}
 			}
 		}
 
-		if (zk->exists(HEALTH_BACKSLASH + id + BLOCKS, exists, error_code)) {
-			if (!exists) {
-				if (!zk->create(HEALTH_BACKSLASH + id + BLOCKS, ZKWrapper::EMPTY_VECTOR, error_code)) {
-					LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/blocks" << error_code;
-				}
-			}
-		}
 
 		// Create an ephemeral node at /health/<datanode_id>/heartbeat
 		// if it doesn't already exist. Should have a ZOPERATIONTIMEOUT
-		if (zk->exists(HEALTH_BACKSLASH + id + BLOCKS + HEARTBEAT, exists, error_code)) {
+		if (zk->exists(HEALTH_BACKSLASH + id + "/heartbeat", exists, error_code)) {
 			if (exists) {
-				if (!zk->delete_node(HEALTH_BACKSLASH + id + BLOCKS + HEARTBEAT, error_code)) {
-					LOG(ERROR) << CLASS_NAME <<  "Failed deleting /health/<data_node_id>/blocks/heartbeat" << error_code;
+				if (!zk->delete_node(HEALTH_BACKSLASH + id + "/heartbeat", error_code)) {
+					LOG(ERROR) << CLASS_NAME <<  "Failed deleting /health/<data_node_id>/heartbeat " << error_code;
 				}
 			}
-			if (!zk->create(HEALTH_BACKSLASH + id + BLOCKS + HEARTBEAT, ZKWrapper::EMPTY_VECTOR, error_code, true)) {
-				LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/blocks/heartbeat" << error_code;
+			if (!zk->create(HEALTH_BACKSLASH + id + "/heartbeat", ZKWrapper::EMPTY_VECTOR, error_code, true)) {
+				LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/heartbeat " << error_code;
 			}
 		}
 
@@ -150,9 +153,18 @@ namespace zkclient{
 		data.resize(sizeof(DataNodePayload));
 		memcpy(&data[0], &data_node_payload, sizeof(DataNodePayload));
 
-		if (!zk->create(HEALTH_BACKSLASH + id + BLOCKS + STATS, data, error_code, false)) {
-			LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/blocks/stats" << error_code;
+		if (!zk->create(HEALTH_BACKSLASH + id + STATS, data, error_code, false)) {
+			LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/stats " << error_code;
 		}
+
+		if (zk->exists(HEALTH_BACKSLASH + id + BLOCKS, exists, error_code)) {
+			if (!exists) {
+				if (!zk->create(HEALTH_BACKSLASH + id + BLOCKS, ZKWrapper::EMPTY_VECTOR, error_code)) {
+					LOG(ERROR) << CLASS_NAME <<  "Failed creating /health/<data_node_id>/blocks " << error_code;
+				}
+			}
+		}
+
 	}
 
 
