af99db2e4f99815df4e0e4bac509b76686371678
Peter Elmers <peter.elmers@yahoo.com>
2016-11-15 11:37:53 -0600

    Add docstrings to NativeFS header.

--- native-filesystem/include/native_filesystem.h
+++ native-filesystem/include/native_filesystem.h
@@ -32,24 +32,63 @@ namespace nativefs {
 
 class NativeFS{
 	public:
+		/**
+		 * Construct a NativeFS backed by the given filename.
+		 */
 		NativeFS(std::string);
+		/**
+		 * Construct a NativeFS by copying fields from other and moving disk
+		 * streams over.
+		 */
 		NativeFS(NativeFS& other);
+		/**
+		 * Destroy NativeFS, free block array.
+		 */
 		~NativeFS();
+		/**
+		 * Write provided block contents to a block with given id. On success,
+		 * return true. Otherwise false (e.g. no free space).
+		 */
 		bool writeBlock(uint64_t, std::string);
+		/**
+		 * Get the contents of the block with given id. If successful, return
+		 * the contents of the block and set provided bool to true. On failure
+		 * to find the block, return empty string and set bool to false.
+		 */
 		std::string getBlock(uint64_t, bool&);
+		/**
+		 * Delete contents of provided block id from this datanode. Return true
+		 * if delete successful, false otherwise (block id not found).
+		 */
 		bool rmBlock(uint64_t);
+		/**
+		 * Return the total capacity of this datanode.
+		 */
 		long getTotalSpace();
+		/**
+		 * Return an estimate of the remaining free space on this datanode.
+		 */
 		long getFreeSpace();
 
 	private:
+		/**
+		 * Attempt to place provided block info in the block list. Return
+		 * whether successful, false if no empty space found in the block list.
+		 */
 		bool addBlock(const block_info& info);
 		/**
 		 * Mark the area of disk from start to end as free.
 		 */
 		void freeRange(uint64_t start, uint64_t end);
 		bool allocateBlock(size_t size, uint64_t& offset);
+		/**
+		 * Persist current block metadata to storage.
+		 */
 		void flushBlocks();
-        void printFreeBlocks();
+		/**
+		 * For debugging, print the free block lists.
+		 */
+		void printFreeBlocks();
 
 		block_info* blocks;
 		mutable std::mutex listMtx;
