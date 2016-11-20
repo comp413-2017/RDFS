4e03b5cf7471ad5c3b0dee8e7f9ec181e30fd8ee
Kyler Mejia <mejia.kyler@gmail.com>
2016-11-20 18:27:37 +0000

    remove some notes, fix indentation, refactor unnecessary if statement

--- zookeeper/source/zk_nn_client.cc
+++ zookeeper/source/zk_nn_client.cc
@@ -60,23 +60,6 @@ namespace zkclient{
         }
     }
 
-    /*
-        - watcher to check for datanodes being added
-        - /health
-        - register watcher on /heartbeat
-    - watcher to check each individual datanode for death
-        - make sure heartbeat disappeared
-        - if so, look at all blocks and put those on replication queue
-            - also add all blocks from replication queue to blocks to be replicated
-            - find datanode with block (read from)
-            - find datanode for block (write to)
-            - put on queue ^
-            - push to vector of ops
-            - recursively delete work queue
-            - recursively delete znode
-            - execute multiop
-    */
-
     /**
      * Static
      */
@@ -246,28 +229,25 @@ namespace zkclient{
 
 
     bool ZkNnClient::previousBlockComplete(uint64_t prev_id) {
-	int error_code;
-	/* this value will eventually be read from config file */
-	int MIN_REPLICATION = 1;
-	std::vector<std::string> children;
-	std::string block_id_str = std::to_string(prev_id);
-	if (zk->get_children(BLOCK_LOCATIONS + block_id_str, children, error_code)) {
-	    if (children.size() >= MIN_REPLICATION){
-		return true;						
-	    }
-	    return false;
-	}
-	return true;
+    	int error_code;
+    	/* this value will eventually be read from config file */
+    	int MIN_REPLICATION = 1;
+    	std::vector<std::string> children;
+    	std::string block_id_str = std::to_string(prev_id);
+    	if (zk->get_children(BLOCK_LOCATIONS + block_id_str, children, error_code)) {
+            return children.size() >= MIN_REPLICATION;
+    	}
+    	return true;
     }
 
     bool ZkNnClient::add_block(AddBlockRequestProto& req, AddBlockResponseProto& res) {
 
-	// make sure previous addBlock operation has completed
-	auto prev_id = req.previous().blockid();
-	if (!previousBlockComplete(prev_id)){
-	    LOG(ERROR) << "Previous Add Block Operation has not finished";
-	    return false;	 
-	}
+    	// make sure previous addBlock operation has completed
+    	auto prev_id = req.previous().blockid();
+    	if (!previousBlockComplete(prev_id)){
+    	    LOG(ERROR) << "Previous Add Block Operation has not finished";
+    	    return false;	 
+    	}
         // Build a new block for the response
         auto block = res.mutable_block();
 
