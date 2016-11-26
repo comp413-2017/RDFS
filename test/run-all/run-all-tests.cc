bc14b2aa9c5eec9b622bf27779186c7c34d65a12
captain695 <cea4@rice.edu>
2016-11-26 12:46:02 -0500

    Removed RunAllTests executable from calling itself

--- test/run-all/run-all-tests.cc
+++ test/run-all/run-all-tests.cc
@@ -5,9 +5,9 @@ int main(int argc, char **argv) {
 	system("~/rdfs/build/test/NameNodeTest");
 	system("~/rdfs/build/test/NativeFsTest");
 	system("~/rdfs/build/test/ReadWriteTest");
-	system("~/rdfs/build/test/RunAllTests");
 	system("~/rdfs/build/test/ZKDNClientTest");
 	system("~/rdfs/build/test/ZKLockTest");
 	system("~/rdfs/build/test/ZKQueueTest");
 	system("~/rdfs/build/test/ZKWrapperTest");
+	return 1;
 }
