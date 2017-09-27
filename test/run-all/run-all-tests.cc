ccb574f82d7cf21ac7ca54f55e21a7b739e93448
Eddie Dugan <eddiedugan4@gmail.com>
2017-09-27 15:39:27 -0500

    add repl test to run-all-tests

--- test/run-all/run-all-tests.cc
+++ test/run-all/run-all-tests.cc
@@ -3,6 +3,7 @@
 // Run all tests in folder and subfolders
 int main(int argc, char **argv) {
 	system("~/rdfs/build/test/DeleteTest");
+	system("~/rdfs/build/test/ReplicationTest");
 	system("~/rdfs/build/test/NameNodeTest");
 	system("~/rdfs/build/test/NativeFsTest");
 	system("~/rdfs/build/test/ReadWriteTest");
