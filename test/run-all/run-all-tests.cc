c534ac68a3e25441e65eb0046aab9c5b07293281
Eddie Dugan <eddiedugan4@gmail.com>
2017-09-27 15:39:27 -0500

    add repl test to run-all-tests

--- test/run-all/run-all-tests.cc
+++ test/run-all/run-all-tests.cc
@@ -2,12 +2,13 @@
 
 // Run all tests in folder and subfolders
 int main(int argc, char **argv) {
-  system("~/rdfs/build/test/DeleteTest");
-  system("~/rdfs/build/test/NameNodeTest");
-  system("~/rdfs/build/test/NativeFsTest");
-  system("~/rdfs/build/test/ReadWriteTest");
-  system("~/rdfs/build/test/ZKDNClientTest");
-  system("~/rdfs/build/test/ZKLockTest");
-  system("~/rdfs/build/test/ZKWrapperTest");
-  return 1;
+	system("~/rdfs/build/test/DeleteTest");
+	system("~/rdfs/build/test/ReplicationTest");
+	system("~/rdfs/build/test/NameNodeTest");
+	system("~/rdfs/build/test/NativeFsTest");
+	system("~/rdfs/build/test/ReadWriteTest");
+	system("~/rdfs/build/test/ZKDNClientTest");
+	system("~/rdfs/build/test/ZKLockTest");
+	system("~/rdfs/build/test/ZKWrapperTest");
+	return 1;
 }
