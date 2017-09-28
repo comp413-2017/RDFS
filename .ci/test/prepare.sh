3cdaa61b6afe4a9231fc722cdd5de751c3f5a8a9
Eddie Dugan <eddiedugan4@gmail.com>
2017-09-28 16:06:20 -0500

    try adding generate_file.py

--- .ci/test/prepare.sh
+++ .ci/test/prepare.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+mkdir /home/vagrant/
+mkdir /home/vagrant/rdfs/
+mkdir /home/vagrant/rdfs/test/
+mkdir /home/vagrant/rdfs/test/integration/
+echo 'for i in range(0, 500000):
+    print "mango" + str(i)' > /home/vagrant/rdfs/test/integration/generate_file.py
+
 mkdir build
 cd build
 cmake ..
