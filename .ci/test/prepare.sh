64537f19ddf0637ed255583099d691d1873c7b12
Kevin Lin <developer@kevinlin.info>
2017-09-28 20:37:15 -0500

    Mimic vagrant home directory structure

--- .ci/test/prepare.sh
+++ .ci/test/prepare.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
 set -ex
-if [ ! -d "/home/vagrant/rdfs" ]; then
-  mkdir /home/vagrant/rdfs/
-fi
-if [ ! -d "/home/vagrant/rdfs/test" ]; then
-  mkdir /home/vagrant/rdfs/test/
-fi
-if [ ! -d "/home/vagrant/rdfs/test/integration" ]; then
-  mkdir /home/vagrant/rdfs/test/integration/
-fi
 
-echo 'for i in range(0, 500000):
-    print "mango" + str(i)' > /home/vagrant/rdfs/test/integration/generate_file.py
+# Mimic the file system structure in the development environment
+sudo ln -s $HOME /home/vagrant
+sudo ln -s $TRAVIS_BUILD_DIR $HOME/rdfs
 
 mkdir build
 cd build
