71b71b0a4edfb2b8e27898c63a204c786e819d34
Kevin Lin <developer@kevinlin.info>
2017-09-29 20:01:43 -0500

    Add encrypted private key to .ci

--- .ci/test/prepare.sh
+++ .ci/test/prepare.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+# Add deploy keys to environment
+mkdir -p ~/.ssh
+openssl aes-256-cbc -K $encrypted_493c05fcd547_key -iv $encrypted_493c05fcd547_iv -in .ci/keys/deploy_id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+
 mkdir build
 cd build
 cmake ..
