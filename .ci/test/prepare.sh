479e56ea520fc6ece753147c9e90a6f51da750f6
Kevin Lin <LINKIWI@users.noreply.github.com>
2017-10-05 14:55:11 -0500

    Lint step in CI (#24)

--- .ci/test/prepare.sh
+++ .ci/test/prepare.sh
@@ -7,6 +7,10 @@ mkdir -p ~/.ssh
 openssl aes-256-cbc -K $encrypted_493c05fcd547_key -iv $encrypted_493c05fcd547_iv -in .ci/keys/deploy_id_rsa.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
+# Install linter
+sudo pip install cpplint==1.3.0
+
+# Create build folders and compile
 mkdir build
 cd build
 cmake ..
