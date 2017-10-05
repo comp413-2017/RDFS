4471f00abc2e2633139fa2c44750f96167ad3da1
Kevin Lin <developer@kevinlin.info>
2017-10-05 13:34:10 -0500

    Lint step in CI

---
+++ .ci/test/lint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+IGNORED_RULES=( build/include_subdir build/c++11 )
+SOURCE_DIRECTORIES=( native-filesystem )
+
+# Build `filter` argument for a list of ignored rules
+rule_filter=""
+for rule in "${IGNORED_RULES[@]}"
+do
+    rule_filter=-${rule},${rule_filter}
+done
+
+# Execute cpplint on all source directories
+for directory in "${SOURCE_DIRECTORIES[@]}"
+do
+    # TODO(LINKIWI): Don't force exit 0 after all lint errors have actually been fixed
+    cpplint \
+        --filter=${rule_filter} \
+        --recursive \
+        ${directory} || :
+done
