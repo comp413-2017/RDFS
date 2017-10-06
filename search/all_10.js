2fa51662d6510feca77a815a2e7cc5fa82049156
Travis CI <travis@travis-ci.org>
2017-10-06 19:40:49 +0000

    [autocommit] [docs] 9ec335e8144389a0f69cbb01b4c240a7407c0208 (build 118)

--- search/all_10.js
+++ search/all_10.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['rice_2dhdfs',['Rice-HDFS',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['rdfs_202_2e0',['RDFS 2.0',['../md__r_e_a_d_m_e.html',1,'']]],
   ['range_5fconst_5fiterator_3c_20pugi_3a_3axml_5fdocument_20_3e',['range_const_iterator&lt; pugi::xml_document &gt;',['../structboost_1_1range__const__iterator_3_01pugi_1_1xml__document_01_4.html',1,'boost']]],
   ['range_5fconst_5fiterator_3c_20pugi_3a_3axml_5fnode_20_3e',['range_const_iterator&lt; pugi::xml_node &gt;',['../structboost_1_1range__const__iterator_3_01pugi_1_1xml__node_01_4.html',1,'boost']]],
   ['range_5fmutable_5fiterator_3c_20pugi_3a_3axml_5fdocument_20_3e',['range_mutable_iterator&lt; pugi::xml_document &gt;',['../structboost_1_1range__mutable__iterator_3_01pugi_1_1xml__document_01_4.html',1,'boost']]],
--- search/pages_1.js
+++ search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['rice_2dhdfs',['Rice-HDFS',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['rdfs_202_2e0',['RDFS 2.0',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
