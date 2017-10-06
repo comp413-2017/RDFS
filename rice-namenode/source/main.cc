6f500eccc71b063d87d0db52544b195e0cf3e8b4
Eddie Dugan <eddiedugan4@gmail.com>
2017-10-06 15:08:52 -0500

    Feature/namenode lint (#32)
    * include lint easy fixes
    
    * name node lint errors
    
    * fixing lint errors in name node, continued
    
    * change non-const references to pointers (this was the fixed that required the least change)
    
    * fixing calls to nn functions in tests
    
    * add namenode dir to lint step in ci

--- rice-namenode/source/main.cc
+++ rice-namenode/source/main.cc
@@ -1,11 +1,13 @@
+// Copyright 2017 Rice University, COMP 413 2017
+
 #define ELPP_FRESH_LOG_FILE
 #define ELPP_THREAD_SAFE
 
+#include <rpcserver.h>
+#include <easylogging++.h>
 #include <cstdlib>
 #include <iostream>
 #include <asio.hpp>
-#include <rpcserver.h>
-#include <easylogging++.h>
 #include "zk_nn_client.h"
 #include "ClientNamenodeProtocolImpl.h"
 #include "HaServiceProtocolImpl.h"
@@ -15,19 +17,28 @@ INITIALIZE_EASYLOGGINGPP
 // Absolute directory from vagrant VM to config file
 #define LOG_CONFIG_FILE "/home/vagrant/rdfs/config/nn-log-conf.conf"
 
-using namespace client_namenode_translator;
+using client_namenode_translator::ClientNamenodeTranslator;
 
 /**
- * Function to parse commandline options and store results into the input pointers.
+ * Function to parse commandline options and store results into the input
+ * pointers.
  * @param argc Count of arguments received at the command line.
  * @param argv Array of character arrays storing arguments.
- * @param xferPort A pointer in which to place the xferPort entered at the command line.
- * @param ipcPort A pointer in which to place the ipcPort entered at the command line.
- * @param verbosity A pointer in which to place the verbosity value entered at the command line.
+ * @param xferPort A pointer in which to place the xferPort entered at the
+ * command line.
+ * @param ipcPort A pointer in which to place the ipcPort entered at the
+ * command line.
+ * @param verbosity A pointer in which to place the verbosity value entered at
+ * the command line.
  * @param backingStore A reference to set to the input backingStore.
  * @return 0 on success, -1 on any error.
  */
-static inline int parse_cmdline_options(int argc, char *argv[], int *port, int *verbosity) {
+static inline int parse_cmdline_options(
+    int argc,
+    char *argv[],
+    int *port,
+    int *verbosity
+) {
   int c;
   char buf[64];
 
@@ -51,17 +62,23 @@ static inline int parse_cmdline_options(int argc, char *argv[], int *port, int *
         break;
       case '?':
         switch (optopt) {
-          case 'v':LOG(ERROR) << "Option -v requires an argument specifying a verbosity level.";
+          case 'v':LOG(ERROR) << "Option -v requires an argument specifying a "
+                "verbosity level.";
             return -1;
-          case 'p':LOG(ERROR) << "Option -p requires an argument specifying an ipc port.";
+          case 'p':LOG(ERROR) << "Option -p requires an argument specifying an "
+                "ipc port.";
             return -1;
           default:
             if (isprint(optopt)) {
-              sprintf(buf, "Unknown option -%c.", optopt);
+              snprintf(buf, sizeof(buf), "Unknown option -%c.", optopt);
               LOG(WARNING) << buf;
               return 0;
             } else {
-              sprintf(buf, "Unknown option character `\\\\x%x'.", optopt);
+              snprintf(
+                  buf,
+                  sizeof(buf),
+                  "Unknown option character `\\\\x%x'.",
+                  optopt);
               LOG(WARNING) << buf;
               return 0;
             }
@@ -90,14 +107,20 @@ int main(int argc, char *argv[]) {
 
   el::Loggers::setVerboseLevel(verbosity);
 
-  auto zk_shared = std::make_shared<ZKWrapper>("localhost:2181,localhost:2182,localhost:2183", error_code, "/testing");
+  auto zk_shared = std::make_shared<ZKWrapper>(
+      "localhost:2181,localhost:2182,localhost:2183",
+      error_code,
+      "/testing");
   zkclient::ZkNnClient nncli(zk_shared);
   nncli.register_watches();
 
   LOG(INFO) << "Namenode is starting";
-  ClientNamenodeTranslator translator(port, nncli);
+  ClientNamenodeTranslator translator(port, &nncli);
   // high availability translator
   RPCServer server = translator.getRPCServer();
-  ha_service_translator::HaServiceTranslator ha_service_translator(&server, nncli, port);
+  ha_service_translator::HaServiceTranslator ha_service_translator(
+      &server,
+      &nncli,
+      port);
   server.serve(io_service);
 }
