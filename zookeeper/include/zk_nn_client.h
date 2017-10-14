3458a696200ae49e6fb44cfe1b656ce1675790a4
Kevin Lin <developer@kevinlin.info>
2017-10-14 18:45:18 -0500

    Set owner HDFS call modifies permissions ACL in ZK; fixes #53

--- zookeeper/include/zk_nn_client.h
+++ zookeeper/include/zk_nn_client.h
@@ -356,6 +356,11 @@ class ZkNnClient : public ZkClientCommon {
                  SetOwnerResponseProto &res,
                  std::string client_name = "default");
   /**
+   * Sets the owner of the file.
+   */
+  bool set_owner(SetOwnerRequestProto &req, SetOwnerResponseProto &res);
+
+  /**
    * Add block.
    * @param req AddBlockRequestProto
    * @param res AddBlockResponseProto
