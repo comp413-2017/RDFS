151c4650cb2cea841e4780a1c42d11da9ebaa23d
Yufeng Zhou <yufeng.zhou@rice.edu>
2017-10-16 11:01:52 -0500

    Set up mkdir test skeleton

---
+++ test/namenode/MkdirTest.cc
@@ -0,0 +1,18 @@
+#include "NameNodeTest.h"
+
+
+TEST_F(NamenodeTest, mkdirDepth1) {
+    //
+}
+
+TEST_F(NamenodeTest, mkdirDepth1024) {
+    //
+}
+
+TEST_F(NamenodeTest, mkdirExistentDirectory) {
+    //
+}
+
+TEST_F(NamenodeTest, mkdirExistentFile) {
+    //
+}
