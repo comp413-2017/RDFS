e0c0b69445d5dc3d518a31d62fc4fff5d4a15bc2
jeffreyxiong <jeffxiong017@gmail.com>
2017-10-18 14:21:11 -0500

    deletion test, one benchmark

--- test/namenode/DeletionTest.cc
+++ test/namenode/DeletionTest.cc
@@ -107,3 +107,58 @@ TEST_F(NamenodeTest, deleteNestedNonexistantFile) {
     ASSERT_EQ(client->destroy(delete_req, delete_resp), zkclient::ZkNnClient::DeleteResponse::FileDoesNotExist);
 
 }
+
+TEST_F(NamenodeTest, benchmarkDeletionDepth) {
+    el::Loggers::setVerboseLevel(9);
+
+    int i = 0;
+    std::vector<int> depths = {5, 100};
+    std::vector<int> dir_nums = {10, 100, 1000};
+
+    for (auto depth : depths) {
+        for (auto dir_num : dir_nums) {
+
+            auto dir_string = "/testing/delete_testing_depth" + std::to_string(depth) +
+                  "_size" + std::to_string(dir_num) + "/";
+
+            for (i = 0; i < depth - 2; i++) {
+                dir_string += std::to_string(i) + "/";
+            }
+
+            for (i = 0; i < dir_num; i++) {
+                hadoop::hdfs::CreateRequestProto create_req;
+                hadoop::hdfs::CreateResponseProto create_resp;
+                create_req.set_src(dir_string + std::to_string(i));
+                create_req.set_clientname("test_client_name");
+                create_req.set_createparent(true);
+                EXPECT_EQ(client->create_file(create_req, create_resp), zkclient::ZkNnClient::CreateResponse::Ok);
+            }
+        }
+    }
+
+    for (auto depth : depths) {
+        for (auto dir_num : dir_nums) {
+            auto timer_string = "delete_depth" +
+                    std::to_string(depth) +
+                    "_size" + std::to_string(dir_num);
+
+            auto dir_string = "/testing/delete_testing_depth" + std::to_string(depth) + \
+                    "_size" + std::to_string(dir_num) + "/";
+
+            for (i = 0; i < depth - 2; i++) {
+                dir_string += std::to_string(i) + "/";
+            }
+
+            TIMED_SCOPE_IF(listingPerformanceObj,
+                    timer_string,
+                    VLOG_IS_ON(9));
+
+            hadoop::hdfs::DeleteRequestProto delete_req;
+            delete_req.set_recursive(true);
+            delete_req.set_src(dir_string);
+            hadoop::hdfs::DeleteResponseProto delete_resp;
+            ASSERT_EQ(client->destroy(delete_req, delete_resp), zkclient::ZkNnClient::DeleteResponse::Ok);
+            ASSERT_FALSE(client->file_exists(src));
+        }
+    }
+}
