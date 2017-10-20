7b086fc35ce42a255a6f573b9011c747b37e2fc1
Kevin Lin <developer@kevinlin.info>
2017-10-20 15:16:40 -0500

    Fix lint errors

--- rice-namenode/source/ClientNamenodeProtocolImpl.cc
+++ rice-namenode/source/ClientNamenodeProtocolImpl.cc
@@ -267,14 +267,6 @@ std::string ClientNamenodeTranslator::setPermission(std::string input) {
   return Serialize(res);
 }
 
-//std::string ClientNamenodeTranslator::modifyAclEntries(std::string input) {
-//    ModifyAclEntriesRequestProto req;
-//    ModifyAclEntriesResponseProto res;
-//    req.ParseFromString(input);
-//    zk->modifyAclEntries(req, res);
-//    logMessage(&res, "ModifyAclEntries");
-//}
-
 std::string ClientNamenodeTranslator::getListing(std::string input) {
   GetListingRequestProto req;
   GetListingResponseProto res;
