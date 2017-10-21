a12683c401c255973527deef62c2d9499ed64acc
Kevin Lin <developer@kevinlin.info>
2017-10-20 20:11:29 -0500

    Cached protobuf build

--- .ci/install/protobuf.sh
+++ .ci/install/protobuf.sh
@@ -2,15 +2,11 @@
 
 set -ex
 
-PROTOBUF_MIRROR=https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-cpp-3.0.0.tar.gz
+PROTOBUF_CACHE=http://kevinlin.web.rice.edu/static/protobuf.zip
 
 cd $HOME
-wget --quiet -O protobuf.tar.gz $PROTOBUF_MIRROR
-tar -xf protobuf.tar.gz
-rm protobuf.tar.gz
+wget --quiet -O protobuf.zip $PROTOBUF_CACHE
+unzip protobuf.zip > /dev/null
 cd protobuf-3.0.0
-./autogen.sh
-./configure --prefix=/usr
-sudo make > /dev/null
-sudo make install
+sudo make install > /dev/null
 sudo ldconfig
