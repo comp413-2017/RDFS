f6d3f9b3b4de12bea2abc78de3a1dbc67edee4fb
Kevin Lin <developer@kevinlin.info>
2017-10-23 18:30:01 -0500

    Cache directories in Travis

--- .ci/install/hadoop.sh
+++ .ci/install/hadoop.sh
@@ -5,8 +5,10 @@ set -ex
 HADOOP_MIRROR=http://kevinlin.web.rice.edu/static/hadoop-2.8.1.tar.gz
 
 cd $HOME
-wget --quiet -O hadoop.tar.gz $HADOOP_MIRROR
-tar -xf hadoop.tar.gz
+if [ ! -f /tmp/cache/protobuf.zip ]; then
+    wget --quiet -O /tmp/cache/hadoop.tar.gz $HADOOP_MIRROR
+fi
+tar -xf /tmp/cache/hadoop.tar.gz
 rm hadoop.tar.gz
 mv hadoop-2.8.1 hadoop
 cp $TRAVIS_BUILD_DIR/config/hdfs-site.xml hadoop/etc/hadoop/hdfs-site.xml
--- .ci/install/protobuf.sh
+++ .ci/install/protobuf.sh
@@ -5,8 +5,10 @@ set -ex
 PROTOBUF_CACHE=http://kevinlin.web.rice.edu/static/protobuf.zip
 
 cd $HOME
-wget --quiet -O protobuf.zip $PROTOBUF_CACHE
-unzip protobuf.zip > /dev/null
+if [ ! -f /tmp/cache/protobuf.zip ]; then
+    wget --quiet -O /tmp/cache/protobuf.zip $PROTOBUF_CACHE
+fi
+unzip /tmp/cache/protobuf.zip > /dev/null
 cd protobuf-3.0.0
 sudo make install > /dev/null
 sudo ldconfig
