e66099eb50a49f23c08fc68011245d4ccf87f7e4
Kevin Lin <LINKIWI@users.noreply.github.com>
2017-10-24 08:20:06 -0500

    Travis build cache for protobuf, hadoop (#96)
    * Cache directories in Travis
    
    * Unnecessary cache directory mkdir
    
    * Also cache zookeeper
    
    * Cache entire hadoop directory
    
    * Cache entire protobuf directory
    
    * Use symbolic links instead of moving out of /tmp/cache

--- .ci/install/hadoop.sh
+++ .ci/install/hadoop.sh
@@ -5,9 +5,11 @@ set -ex
 HADOOP_MIRROR=http://kevinlin.web.rice.edu/static/hadoop-2.8.1.tar.gz
 
 cd $HOME
-wget --quiet -O hadoop.tar.gz $HADOOP_MIRROR
-tar -xf hadoop.tar.gz
-rm hadoop.tar.gz
-mv hadoop-2.8.1 hadoop
+if [ ! -d /tmp/cache/hadoop ]; then
+    wget --quiet -O hadoop.tar.gz $HADOOP_MIRROR
+    tar -xf hadoop.tar.gz -C /tmp
+    mv /tmp/hadoop-2.8.1 /tmp/cache/hadoop
+fi
+ln -s /tmp/cache/hadoop hadoop
 cp $TRAVIS_BUILD_DIR/config/hdfs-site.xml hadoop/etc/hadoop/hdfs-site.xml
 cp $TRAVIS_BUILD_DIR/config/core-site.xml hadoop/etc/hadoop/core-site.xml
