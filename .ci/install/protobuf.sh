3fab03066ce835df7840fe3e20d02bd84c9bcd6d
Kevin Lin <developer@kevinlin.info>
2017-10-23 19:34:07 -0500

    Cache entire protobuf directory

--- .ci/install/protobuf.sh
+++ .ci/install/protobuf.sh
@@ -5,10 +5,12 @@ set -ex
 PROTOBUF_CACHE=http://kevinlin.web.rice.edu/static/protobuf.zip
 
 cd $HOME
-if [ ! -f /tmp/cache/protobuf.zip ]; then
-    wget --quiet -O /tmp/cache/protobuf.zip $PROTOBUF_CACHE
+if [ ! -d /tmp/cache/protobuf ]; then
+    wget --quiet -O protobuf.zip $PROTOBUF_CACHE
+    unzip protobuf.zip > /dev/null
+    mv protobuf-3.0.0 /tmp/cache/protobuf
 fi
-unzip /tmp/cache/protobuf.zip > /dev/null
+mv /tmp/cache/protobuf protobuf-3.0.0
 cd protobuf-3.0.0
 sudo make install > /dev/null
 sudo ldconfig
