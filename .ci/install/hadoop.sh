59beede194167d811cc879356137dc26f5b28f7e
Kevin Lin <developer@kevinlin.info>
2017-10-23 19:22:40 -0500

    Cache entire hadoop directory

--- .ci/install/hadoop.sh
+++ .ci/install/hadoop.sh
@@ -5,10 +5,11 @@ set -ex
 HADOOP_MIRROR=http://kevinlin.web.rice.edu/static/hadoop-2.8.1.tar.gz
 
 cd $HOME
-if [ ! -f /tmp/cache/hadoop.tar.gz ]; then
-    wget --quiet -O /tmp/cache/hadoop.tar.gz $HADOOP_MIRROR
+if [ ! -d /tmp/cache/hadoop ]; then
+    wget --quiet -O hadoop.tar.gz $HADOOP_MIRROR
+    tar -xf hadoop.tar.gz -C /tmp
+    mv /tmp/hadoop-2.8.1 /tmp/cache/hadoop
 fi
-tar -xf /tmp/cache/hadoop.tar.gz
-mv hadoop-2.8.1 hadoop
+mv /tmp/cache/hadoop hadoop
 cp $TRAVIS_BUILD_DIR/config/hdfs-site.xml hadoop/etc/hadoop/hdfs-site.xml
 cp $TRAVIS_BUILD_DIR/config/core-site.xml hadoop/etc/hadoop/core-site.xml
