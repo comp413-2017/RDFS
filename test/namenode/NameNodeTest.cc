b84c2ca4cb2c664986f1aa3201ccbb3e8fb0a87c
cannon <cannon10100@gmail.com>
2017-10-24 11:42:34 -0500

    Disable INFO logging in an effort to abbreviate logs during testing

--- test/namenode/NameNodeTest.cc
+++ test/namenode/NameNodeTest.cc
@@ -277,6 +277,7 @@ TEST_F(NamenodeTest, testRenameDirWithFiles) {
 
 int main(int argc, char **argv) {
     el::Configurations conf(LOG_CONFIG_FILE);
+    conf.set(el::Level::Info, el::ConfigurationType::Enabled, "falses");
     el::Loggers::reconfigureAllLoggers(conf);
     el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
 
