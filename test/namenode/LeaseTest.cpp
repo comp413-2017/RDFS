8044ba4c9ad37137894bba20009342b5de205876
jeffreyxiong <jeffxiong017@gmail.com>
2017-10-25 15:34:42 -0500

    lint fix

--- test/namenode/LeaseTest.cpp
+++ test/namenode/LeaseTest.cpp
@@ -2,12 +2,10 @@
 
 #include "NameNodeTest.h"
 
-
 // Ensure that a lease is created successfully and that it does its job,
 // by making sure another client can't request a lease on the same file
 
 TEST_F(NamenodeTest, issueLeaseTest) {
-
     // Open a lease on some file, through an append request
     // Check append status
 
@@ -19,7 +17,6 @@ TEST_F(NamenodeTest, issueLeaseTest) {
 }
 
 TEST_F(NamenodeTest, renewLeaseTest) {
-
     // Open a lease on some file, through an append request
     // Check append status
 
@@ -29,5 +26,6 @@ TEST_F(NamenodeTest, renewLeaseTest) {
     ASSERT_TRUE(true);
 }
 
-// Performance could be measured by tracking the round-trip time of lease open and lease close
+// Performance could be measured by tracking the round-trip time of lease open
+// and lease close
 // Performance could be measured by tracking the time it takes to issue a lease
