f34943fb763030ec791b869645a3fef5b7bedeff
Kyungchan Koh <kyungchan.koh@gmail.com>
2017-10-31 00:21:08 -0500

    Added making of the ISAL library into .ci/install/isal.sh

--- .ci/install/isal.sh
+++ .ci/install/isal.sh
@@ -9,5 +9,8 @@ if [ ! -f /tmp/cache/isal ]; then
     wget --quiet -O isal.tar.gz $ISAL_MIRROR
     tar -xf isal.tar.gz -C /tmp
     mv /tmp/isa-l_open_src_2.13 /tmp/cache/isal
+    cd /tmp/cache/isal
+    make
+    cd $HOME
 fi
 ln -s /tmp/cache/isal isal
