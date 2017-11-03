d7274da27cf466878abb34ee26f6a3377b4d8191
Kyungchan Koh <kkc6196@gmail.com>
2017-11-02 21:14:21 -0500

    Fixed error with vagrant provisioning! (#137)
    * Fixed error with vagrant provisioning!
    
    * Fixed an error in travis building when downloading the ISAL.

--- .ci/install/isal.sh
+++ .ci/install/isal.sh
@@ -5,7 +5,7 @@ set -ex
 ISAL_MIRROR=http://kevinlin.web.rice.edu/static/isal.tar.gz
 
 cd $HOME
-if [ ! -f /tmp/cache/isal ]; then
+if [ ! -d /tmp/cache/isal ]; then
     wget --quiet -O isal.tar.gz $ISAL_MIRROR
     tar -xf isal.tar.gz -C /tmp
     mv /tmp/isa-l_open_src_2.13 /tmp/cache/isal
