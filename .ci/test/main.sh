718de76f183fd021922a1b79096301a9505c9915
Victoria Nazari <victorianazari@gmail.com>
2017-11-04 15:05:55 -0500

    Add basic structure for webRequestTranslator and tests

--- .ci/test/main.sh
+++ .ci/test/main.sh
@@ -21,6 +21,7 @@ run_test ./DeleteTest
 run_test "./NameNodeTest --gtest_filter=-*Performance*"
 run_test ./NativeFsTest
 run_test "./StorageTest --gtest_filter=-*Time"
+run_test ./WebRequestTranslatorTest
 run_test ./ZKDNClientTest
 run_test ./ZKLockTest
 run_test ./ZKWrapperTest
---
+++ test/webRDFS/WebRequestTranslatorTest.cc
@@ -0,0 +1,36 @@
+// Copyright 2017 Rice University, COMP 413 2017
+
+#define ELPP_THREAD_SAFE
+
+#include <easylogging++.h>
+
+#include <gtest/gtest.h>
+#include "webRequestTranslator.h"
+
+#include <iostream>
+
+INITIALIZE_EASYLOGGINGPP
+
+namespace {
+  TEST(webRequestTranslator, testConvertToResponse) {
+    hadoop::hdfs::DeleteResponseProto delete_resp;
+    webRequestTranslator::convertToResponse(delete_resp);
+}
+
+}
+
+int main(int argc, char **argv) {
+    // Start up zookeeper
+    system("sudo /home/vagrant/zookeeper/bin/zkServer.sh stop");
+    system("sudo /home/vagrant/zookeeper/bin/zkServer.sh start");
+    sleep(10);
+
+    // Initialize and run the tests
+    ::testing::InitGoogleTest(&argc, argv);
+    int res = RUN_ALL_TESTS();
+
+    // Remove test files and shutdown zookeeper
+    system("~/zookeeper/bin/zkCli.sh rmr /testing");
+    system("sudo /home/vagrant/zookeeper/bin/zkServer.sh stop");
+    return res;
+}
