f8828713b8a192e19c8df2e8e8752a50bd535e4b
jsu100 <jhsu2696@gmail.com>
2017-11-12 13:46:17 -0600

    Added in append_file and helpers code without changing get_block_locations

--- zookeeper/include/zk_client_common.h
+++ zookeeper/include/zk_client_common.h
@@ -11,35 +11,40 @@
 
 namespace zkclient {
 
-class ZkClientCommon {
- public:
-  explicit ZkClientCommon(std::string hostAndIp);
-  explicit ZkClientCommon(std::shared_ptr<ZKWrapper> zk);
+    class ZkClientCommon {
+    public:
+        explicit ZkClientCommon(std::string hostAndIp);
+        explicit ZkClientCommon(std::shared_ptr<ZKWrapper> zk);
 
-  void init();
-  std::shared_ptr<ZKWrapper> zk;
+        void init();
+        std::shared_ptr<ZKWrapper> zk;
 
-  // constants used by the clients
-  static const char NAMESPACE_PATH[];
-  static const char BLOCKS_TREE[];
-  static const char HEALTH[];
-  static const char HEALTH_BACKSLASH[];
-  static const char STATS[];
-  static const char HEARTBEAT[];
-  static const char WORK_QUEUES[];
-  static const char REPLICATE_QUEUES[];
-  static const char REPLICATE_QUEUES_NO_BACKSLASH[];
-  static const char DELETE_QUEUES[];
-  static const char DELETE_QUEUES_NO_BACKSLASH[];
-  static const char WAIT_FOR_ACK[];
-  static const char WAIT_FOR_ACK_BACKSLASH[];
-  static const char REPLICATE_BACKSLASH[];
-  static const char BLOCK_LOCATIONS[];
-  static const char BLOCKS[];
+        // constants used by the clients
+        static const char NAMESPACE_PATH[];
+        static const char BLOCKS_TREE[];
+        static const char HEALTH[];
+        static const char HEALTH_BACKSLASH[];
+        static const char STATS[];
+        static const char HEARTBEAT[];
+        static const char WORK_QUEUES[];
+        static const char REPLICATE_QUEUES[];
+        static const char REPLICATE_QUEUES_NO_BACKSLASH[];
+        static const char DELETE_QUEUES[];
+        static const char DELETE_QUEUES_NO_BACKSLASH[];
+        static const char WAIT_FOR_ACK[];
+        static const char WAIT_FOR_ACK_BACKSLASH[];
+        static const char REPLICATE_BACKSLASH[];
+        static const char BLOCK_LOCATIONS[];
+        static const char BLOCKS[];
 
- private:
-  static const std::string CLASS_NAME;
-};
+        // ---------- MJP Header -----------
+        static const char CLIENTS[];
+        static const char LEASES[];
+        // ---------- MJP Footer -----------
+
+    private:
+        static const std::string CLASS_NAME;
+    };
 }  // namespace zkclient
 
 #endif  // ZOOKEEPER_INCLUDE_ZK_CLIENT_COMMON_H_
--- zookeeper/source/zk_client_common.cc
+++ zookeeper/source/zk_client_common.cc
@@ -1,99 +1,50 @@
 // Copyright 2017 Rice University, COMP 413 2017
 
-#ifndef RDFS_ZKCLIENTCOMMON_CC
-#define RDFS_ZKCLIENTCOMMON_CC
+#ifndef ZOOKEEPER_INCLUDE_ZK_CLIENT_COMMON_H_
+#define ZOOKEEPER_INCLUDE_ZK_CLIENT_COMMON_H_
 
-#include "../include/zk_client_common.h"
-
-#include <easylogging++.h>
 #include <zkwrapper.h>
-#include <iostream>
+
+#include <string>
+
+#include <boost/shared_ptr.hpp>
 
 namespace zkclient {
 
-const char ZkClientCommon::WORK_QUEUES[] = "/work_queues/";
-const char ZkClientCommon::REPLICATE_QUEUES[] = "/work_queues/replicate/";
-const char ZkClientCommon::
-        REPLICATE_QUEUES_NO_BACKSLASH[] = "/work_queues/replicate";
-const char ZkClientCommon::DELETE_QUEUES[] = "/work_queues/delete/";
-const char ZkClientCommon::DELETE_QUEUES_NO_BACKSLASH[] = "/work_queues/delete";
-const char ZkClientCommon::WAIT_FOR_ACK[] = "wait_for_acks";
-const char ZkClientCommon::WAIT_FOR_ACK_BACKSLASH[] = "wait_for_acks/";
-const char ZkClientCommon::REPLICATE_BACKSLASH[] = "replicate/";
-const char ZkClientCommon::NAMESPACE_PATH[] = "/fileSystem";
-const char ZkClientCommon::BLOCKS_TREE[] = "/blocks";
-const char ZkClientCommon::HEALTH[] = "/health";
-const char ZkClientCommon::HEALTH_BACKSLASH[] = "/health/";
-const char ZkClientCommon::STATS[] = "/stats";
-const char ZkClientCommon::HEARTBEAT[] = "/heartbeat";
-const char ZkClientCommon::BLOCK_LOCATIONS[] = "/block_locations/";
-const char ZkClientCommon::BLOCKS[] = "/blocks";
+    class ZkClientCommon {
+    public:
+        explicit ZkClientCommon(std::string hostAndIp);
+        explicit ZkClientCommon(std::shared_ptr<ZKWrapper> zk);
 
-ZkClientCommon::ZkClientCommon(std::string hostAndIp) {
-  int error_code;
-  zk = std::make_shared<ZKWrapper>(hostAndIp, error_code, "/testing");
-  init();
-}
+        void init();
+        std::shared_ptr<ZKWrapper> zk;
 
-ZkClientCommon::ZkClientCommon(std::shared_ptr<ZKWrapper> zk_in) : zk(zk_in) {
-  init();
-}
+        // constants used by the clients
+        static const char NAMESPACE_PATH[];
+        static const char BLOCKS_TREE[];
+        static const char HEALTH[];
+        static const char HEALTH_BACKSLASH[];
+        static const char STATS[];
+        static const char HEARTBEAT[];
+        static const char WORK_QUEUES[];
+        static const char REPLICATE_QUEUES[];
+        static const char REPLICATE_QUEUES_NO_BACKSLASH[];
+        static const char DELETE_QUEUES[];
+        static const char DELETE_QUEUES_NO_BACKSLASH[];
+        static const char WAIT_FOR_ACK[];
+        static const char WAIT_FOR_ACK_BACKSLASH[];
+        static const char REPLICATE_BACKSLASH[];
+        static const char BLOCK_LOCATIONS[];
+        static const char BLOCKS[];
 
-void ZkClientCommon::init() {
-  LOG(INFO) << "Initializing ZkClientCommon";
-  auto vec = ZKWrapper::get_byte_vector("");
+        // ---------- MJP Header -----------
+        static const char CLIENTS[];
+        static const char LEASES[];
+        // ---------- MJP Footer -----------
 
-  bool exists;
-  int error_code;
-  // TODO(2016): Add in error handling for failures
-  if (zk->exists("/health", exists, error_code)) {
-    if (!exists) {
-      zk->create("/health", vec, error_code, false);
-    }
-  } else {
-    // TODO(2016): Handle error
-  }
-  if (zk->exists("/fileSystem", exists, error_code)) {
-    if (!exists) {
-      zk->create("/fileSystem", vec, error_code, false);
-    } else {
-    }
-  } else {
-    // TODO(2016): Handle error
-  }
-  if (!zk->recursive_create("/work_queues/wait_for_acks",
-                            ZKWrapper::EMPTY_VECTOR, error_code)) {
-    LOG(ERROR) << "Failed creating /work_queues/wait_for_acks: " << error_code;
-  }
-  // Ensure work_queues exist
-  if (zk->exists(DELETE_QUEUES_NO_BACKSLASH, exists, error_code)) {
-    if (!exists) {
-      if (!zk->create(DELETE_QUEUES_NO_BACKSLASH, ZKWrapper::EMPTY_VECTOR,
-                      error_code, false)) {
-        // Handle failed to create replicate node
-        LOG(INFO) << "Creation failed for delete ueue";;
-      }
-    }
-  }
-  if (zk->exists(REPLICATE_QUEUES_NO_BACKSLASH, exists, error_code)) {
-    if (!exists) {
-      if (!zk->create(REPLICATE_QUEUES_NO_BACKSLASH, ZKWrapper::EMPTY_VECTOR,
-                      error_code, false)) {
-        // Handle failed to create replicate node
-        LOG(INFO) << "Creation failed for repl queue";;
-      }
-    }
-  }
-  if (zk->exists("/block_locations", exists, error_code)) {
-    if (!exists) {
-      zk->create("/block_locations", vec, error_code, false);
-    }
-  } else {
-    // TODO(2016): Handle error
-  }
-
-  LOG(INFO) << "Finished ZkClientCommon";
-}
+    private:
+        static const std::string CLASS_NAME;
+    };
 }  // namespace zkclient
 
-#endif
+#endif  // ZOOKEEPER_INCLUDE_ZK_CLIENT_COMMON_H_
