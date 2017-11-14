4ccf4d8cf7fb71bfb9b2298598189268f48a1eca
jsu100 <jhsu2696@gmail.com>
2017-11-13 23:05:46 -0600

    Fixing errors with merging develop

--- zookeeper/source/zk_client_common.cc
+++ zookeeper/source/zk_client_common.cc
@@ -14,7 +14,7 @@ namespace zkclient {
 const char ZkClientCommon::WORK_QUEUES[] = "/work_queues/";
 const char ZkClientCommon::REPLICATE_QUEUES[] = "/work_queues/replicate/";
 const char ZkClientCommon::
-        REPLICATE_QUEUES_NO_BACKSLASH[] = "/work_queues/replicate";
+    REPLICATE_QUEUES_NO_BACKSLASH[] = "/work_queues/replicate";
 const char ZkClientCommon::DELETE_QUEUES[] = "/work_queues/delete/";
 const char ZkClientCommon::DELETE_QUEUES_NO_BACKSLASH[] = "/work_queues/delete";
 const char ZkClientCommon::WAIT_FOR_ACK[] = "wait_for_acks";
@@ -25,40 +25,40 @@ const char ZkClientCommon::BLOCKS_TREE[] = "/blocks";
 const char ZkClientCommon::HEALTH[] = "/health";
 const char ZkClientCommon::HEALTH_BACKSLASH[] = "/health/";
 const char ZkClientCommon::STATS[] = "/stats";
+const char ZkClientCommon::HEARTBEAT[] = "/heartbeat";
+const char ZkClientCommon::BLOCK_LOCATIONS[] = "/block_locations/";
+const char ZkClientCommon::BLOCKS[] = "/blocks";
+const char ZkClientCommon::LEASES[] = "/leases";
+const char ZkClientCommon::CLIENTS[] = "/clients";
 
+ZkClientCommon::ZkClientCommon(std::string hostAndIp) {
+  int error_code;
+  zk = std::make_shared<ZKWrapper>(hostAndIp, error_code, "/testing");
+  init();
+}
 
-  ZkClientCommon::ZkClientCommon(std::string hostAndIp) {
-    int error_code;
-    zk = std::make_shared<ZKWrapper>(hostAndIp, error_code, "/testing");
-    init();
-  }
+ZkClientCommon::ZkClientCommon(std::shared_ptr<ZKWrapper> zk_in) : zk(zk_in) {
+  init();
+}
 
-  ZkClientCommon::ZkClientCommon(std::shared_ptr<ZKWrapper> zk_in)
-      : zk(zk_in) {
-    init();
-  }
+void ZkClientCommon::init() {
+  LOG(INFO) << "Initializing ZkClientCommon";
+  auto vec = ZKWrapper::get_byte_vector("");
 
-  void ZkClientCommon::init() {
-    LOG(INFO) << "Initializing ZkClientCommon";
-    auto vec = ZKWrapper::get_byte_vector("");
-
-    bool exists;
-    int error_code;
-    // TODO(2016): Add in error handling for failures
-    if (zk->exists("/health", exists, error_code)) {
-      if (!exists) {
-        zk->create("/health", vec, error_code, false);
-      }
-    } else {
-      // TODO(2016): Handle error
+  bool exists;
+  int error_code;
+  // TODO(2016): Add in error handling for failures
+  if (zk->exists("/health", exists, error_code)) {
+    if (!exists) {
+      zk->create("/health", vec, error_code, false);
     }
-    if (zk->exists("/fileSystem", exists, error_code)) {
-      if (!exists) {
-        zk->create("/fileSystem", vec, error_code, false);
-      } else {
-      }
+  } else {
+    // TODO(2016): Handle error
+  }
+  if (zk->exists("/fileSystem", exists, error_code)) {
+    if (!exists) {
+      zk->create("/fileSystem", vec, error_code, false);
     } else {
-      // TODO(2016): Handle error
     }
   } else {
     // TODO(2016): Handle error
@@ -83,27 +83,27 @@ const char ZkClientCommon::STATS[] = "/stats";
         // Handle failed to create replicate node
         LOG(INFO) << "Creation failed for delete ueue";;
       }
-      }
     }
-    if (zk->exists(REPLICATE_QUEUES_NO_BACKSLASH, exists, error_code)) {
-      if (!exists) {
-        if (!zk->create(REPLICATE_QUEUES_NO_BACKSLASH,
-                ZKWrapper::EMPTY_VECTOR, error_code, false)) {
-          // Handle failed to create replicate node
-          LOG(INFO) << "Creation failed for repl queue";;
-        }
-      }
-    }
-    if (zk->exists("/block_locations", exists, error_code)) {
-      if (!exists) {
-        zk->create("/block_locations", vec, error_code, false);
-      }
-    } else {
-      // TODO(2016): Handle error
-    }
-
-    LOG(INFO) << "Finished ZkClientCommon";
   }
+  if (zk->exists(REPLICATE_QUEUES_NO_BACKSLASH, exists, error_code)) {
+    if (!exists) {
+      if (!zk->create(REPLICATE_QUEUES_NO_BACKSLASH, ZKWrapper::EMPTY_VECTOR,
+                      error_code, false)) {
+        // Handle failed to create replicate node
+        LOG(INFO) << "Creation failed for repl queue";;
+      }
+    }
+  }
+  if (zk->exists("/block_locations", exists, error_code)) {
+    if (!exists) {
+      zk->create("/block_locations", vec, error_code, false);
+    }
+  } else {
+    // TODO(2016): Handle error
+  }
+
+  LOG(INFO) << "Finished ZkClientCommon";
+}
 }  // namespace zkclient
 
 #endif
