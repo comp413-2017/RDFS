4da8c1c4bb16aeab9ace31fa343b6af8f89ac2db
cannon <cannon10100@gmail.com>
2017-11-16 14:20:06 -0600

    Changed flush default argument to false. Operations needing a blocking sync should explicitly pass true.

--- zkwrapper/include/zkwrapper.h
+++ zkwrapper/include/zkwrapper.h
@@ -361,7 +361,7 @@ class ZKWrapper {
    * @param synchronous Whether this operation is blocking
    * @return true on success
    */
-  bool flush(const std::string &full_path, bool synchronous = true) const;
+  bool flush(const std::string &full_path, bool synchronous = false) const;
 
   void close();
 
