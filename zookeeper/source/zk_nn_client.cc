51b76d2804c1458af194763a125fce1f1400066c
Daniel Balkum <aeb6@rice.edu>
2017-11-16 21:42:49 -0600

    added some forgotten files

--- zookeeper/source/zk_nn_client.cc
+++ zookeeper/source/zk_nn_client.cc
@@ -1083,7 +1083,6 @@ ZkNnClient::DeleteResponse ZkNnClient::destroy(
     DeleteResponseProto &response,
     std::string client_name) {
   int error_code;
-  int error_code;
   const std::string &path = request.src();
   bool recursive = request.recursive();
   response.set_result(true);
@@ -1864,22 +1863,6 @@ void ZkNnClient::set_file_info(HdfsFileStatusProto *status,
 
     // Write the modified node back to Zookeeper
     zk->set(ZookeeperFilePath(path), zk_data, zk_error);
-
-    if (zk_error != ZK_ERRORS::OK) {
-      LOG(ERROR) << "ZK reported error writing modified node back to disk";
-      return false;
-    }
-
-    return true;
-  }
-
-    if (zk_error != ZK_ERRORS::OK) {
-      LOG(ERROR) << "ZK reported error writing modified node back to disk";
-      return false;
-    }
-
-    return true;
-  }
 
     if (zk_error != ZK_ERRORS::OK) {
       LOG(ERROR) << "ZK reported error writing modified node back to disk";
