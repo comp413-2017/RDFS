ff4caa31f72921156b2974e7f7b40fafbedff2d1
Kyungchan Koh <kyungchan.koh@gmail.com>
2017-11-19 15:59:59 -0600

    Updated travis ci install script for isal.

--- .ci/install/isal.sh
+++ .ci/install/isal.sh
@@ -2,15 +2,16 @@
 
 set -ex
 
-ISAL_MIRROR=http://kevinlin.web.rice.edu/static/isal.tar.gz
+ISAL_MIRROR=http://kevinlin.web.rice.edu/static/isal-2.tar.gz
 
 cd $HOME
-if [ ! -d /tmp/cache/isal ]; then
-    wget --quiet -O isal.tar.gz $ISAL_MIRROR
-    tar -xf isal.tar.gz -C /tmp
-    mv /tmp/isa-l_open_src_2.13 /tmp/cache/isal
-    cd /tmp/cache/isal
-    make
-    cd $HOME
+if [ -d /tmp/cache/isal ]; then
+    rm -rf /tmp/cache/isal
 fi
+wget --quiet -O isal.tar.gz $ISAL_MIRROR
+tar -xf isal.tar.gz -C /tmp
+mv /tmp/isa-l_open_src_2.13 /tmp/cache/isal
+cd /tmp/cache/isal
+make && make install
+cd $HOME
 ln -s /tmp/cache/isal isal
