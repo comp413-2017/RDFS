0b3c8f76ce3f5b2da481798cd33d27584b189acc
Kyungchan Koh <kyungchan.koh@gmail.com>
2017-11-19 21:33:43 -0600

    Changed isal installation for travis builds to not use cache

--- .ci/install/isal.sh
+++ .ci/install/isal.sh
@@ -5,19 +5,12 @@ set -ex
 ISAL_MIRROR=http://kevinlin.web.rice.edu/static/isal-2.tar.gz
 
 cd $HOME
-if [ -d /tmp/cache/isal ]; then
-    cd /tmp/cache/isal
-    make clean
-    cd $HOME
-    rm -rf /tmp/cache/isal
-fi
 wget --quiet -O isal.tar.gz $ISAL_MIRROR
-tar -xf isal.tar.gz -C /tmp
-mv /tmp/isal /tmp/cache/isal
-cd /tmp/cache/isal
+tar -xf isal.tar.gz
+cd $HOME/isal
 ./autogen.sh
 ./configure
 make
 sudo make install
 cd $HOME
-ln -s /tmp/cache/isal isal
+ln -s $HOME/isal isal
