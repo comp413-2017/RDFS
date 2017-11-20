24958c1e61e206b8b25ee12e730e1fd54cc1ff86
Kyungchan Koh <kyungchan.koh@gmail.com>
2017-11-19 18:09:15 -0600

    Fixed typo in filename for new hadoop3 tar file.

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -32,7 +32,7 @@ if [ -d /home/vagrant/hadoop3 ]; then
     rm -rf /home/vagrant/hadoop3
 fi
 wget --quiet http://kevinlin.web.rice.edu/static/hadoop-3.0.0-beta1-2.tar.gz
-tar -xf hadoop-3.0.0-beta1.tar.gz
+tar -xf hadoop-3.0.0-beta1-2.tar.gz
 mv hadoop-3.0.0-beta1 /home/vagrant/hadoop3
 rm hadoop-3.0.0-beta1.tar.gz
 ln -s hadoop3 hadoop
