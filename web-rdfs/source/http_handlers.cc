cda37d36580f4aec96029c7b9fbda506a7dfe7e1
Victoria Nazari <victorianazari@gmail.com>
2017-11-20 21:48:11 -0600

    Feature/webrdfs api change (#201)
    * Fix api issues with webRDFS
    
    * Add destination to rename path
    
    * Add helper functions

--- web-rdfs/source/http_handlers.cc
+++ web-rdfs/source/http_handlers.cc
@@ -12,6 +12,35 @@ void setZk(zkclient::ZkNnClient *zk_arg) {
   zk = zk_arg;
 }
 
+std::string get_request_type(std::shared_ptr<HttpsServer::Request> request) {
+  // Remove op= from query string
+  std::string typeOfRequest = request->query_string.substr(3, 6);
+
+  LOG(DEBUG) << "Type of Request " << typeOfRequest;
+
+  return typeOfRequest;
+}
+
+std::string get_path(std::shared_ptr<HttpsServer::Request> request) {
+  std::string baseUrl = "/webhdfs/v1";
+  int idxOfSplit = (request->path).rfind(baseUrl) + baseUrl.size();
+  std::string path = (request->path).substr(idxOfSplit);
+
+  LOG(DEBUG) << "Path given " << path;
+
+  return path;
+}
+
+std::string get_destination(std::shared_ptr<HttpsServer::Request> request) {
+  std::string destDelim = "&destination=";
+  int idxOfDest = request->query_string.rfind(destDelim) + destDelim.size();
+  std::string dest = request->query_string.substr(idxOfDest);
+
+  LOG(DEBUG) << "Destination given " << dest;
+
+  return dest;
+}
+
 void create_file_handler(std::shared_ptr<HttpsServer::Response> response,
                          std::shared_ptr<HttpsServer::Request> request) {
   LOG(DEBUG) << "HTTP request: create_file_handler";
@@ -92,46 +121,46 @@ void rename_file_handler(std::shared_ptr<HttpsServer::Response> response,
 
 void get_handler(std::shared_ptr<HttpsServer::Response> response,
                  std::shared_ptr<HttpsServer::Request> request) {
-  // TODO(security): invoke another handler depending on qs opcode.
-  std::string baseUrl = "/webhdfs/v1";
+  std::string typeOfRequest = get_request_type(request);
+  std::string path = get_path(request);
 
-  int idxOfSplit = (request->path).rfind(baseUrl) + baseUrl.size();
-  std::string path = (request->path).substr(idxOfSplit);
-
-  // Remove op= from query string
-  std::string typeOfRequest = request->query_string.substr(3);
-
-  LOG(DEBUG) << "Type of Request " << typeOfRequest;
-  LOG(DEBUG) << "Path " << path;
-
-  if (!typeOfRequest.compare("DELETE")) {
-    delete_file_handler(response, path);
-  } else if (!typeOfRequest.compare("OPEN")) {
+  if (!typeOfRequest.compare("OPEN")) {
     read_file_handler(response, path);
-  } else if (!typeOfRequest.compare("MKDIR")) {
-    mkdir_handler(response, path);
-  } else if (!typeOfRequest.find("RENAME")) {
-    std::string pathForRename = typeOfRequest.substr(6);
-    rename_file_handler(response, path, pathForRename);
   } else {
-    create_file_handler(response, request);
+    response->write(SimpleWeb::StatusCode::client_error_bad_request);
   }
 }
 
 void post_handler(std::shared_ptr<HttpsServer::Response> response,
                   std::shared_ptr<HttpsServer::Request> request) {
-  // TODO(security): invoke another handler depending on qs opcode.
-  create_file_handler(response, request);
+  // Do not support post requests right now
+  response->write(SimpleWeb::StatusCode::client_error_bad_request);
 }
 
 void put_handler(std::shared_ptr<HttpsServer::Response> response,
                  std::shared_ptr<HttpsServer::Request> request) {
-  // TODO(security): invoke another handler depending on qs opcode.
-  create_file_handler(response, request);
+  std::string typeOfRequest = get_request_type(request);
+  std::string path = get_path(request);
+
+  if (!typeOfRequest.compare("MKDIRS")) {
+    mkdir_handler(response, path);
+  } else if (!typeOfRequest.compare("RENAME")) {
+    std::string pathForRename = get_destination(request);
+    rename_file_handler(response, path, pathForRename);
+  } else {
+    response->write(SimpleWeb::StatusCode::client_error_bad_request);
+  }
 }
 
 void delete_handler(std::shared_ptr<HttpsServer::Response> response,
                     std::shared_ptr<HttpsServer::Request> request) {
-  // TODO(security): invoke another handler depending on qs opcode.
-  create_file_handler(response, request);
+  // Remove op= from query string
+  std::string typeOfRequest = get_request_type(request);
+  std::string path = get_path(request);
+
+  if (!typeOfRequest.compare("DELETE")) {
+    delete_file_handler(response, path);
+  } else {
+    response->write(SimpleWeb::StatusCode::client_error_bad_request);
+  }
 }
