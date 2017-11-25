60eb688dd81ec75b10e1e0c813c34ac8d2a5d7a0
Kevin Lin <LINKIWI@users.noreply.github.com>
2017-11-25 12:27:17 -0600

    [WIP] Add webRDFS SSL cert as root CA (#203)
    * Regenerated SSL certs with comp413.local CN
    
    * SSL root CA in CI
    
    * [WIP] Use comp413.local in tests without --insecure
    
    * Use comp413.local in all tests

--- .ci/test/prepare.sh
+++ .ci/test/prepare.sh
@@ -11,6 +11,10 @@ mkdir -p ~/.ssh
 openssl aes-256-cbc -K $encrypted_493c05fcd547_key -iv $encrypted_493c05fcd547_iv -in .ci/keys/deploy_id_rsa.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
+# Add SSL root CA to environment
+sudo cp ~/rdfs/config/keys/server.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates
+
 # Install linter
 sudo pip install cpplint==1.3.0
 
