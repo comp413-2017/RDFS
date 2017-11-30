23b1cd5d3af4ba28620ecb82dd8f679f7f9b0b86
Eddie Dugan <eddiedugan4@gmail.com>
2017-11-30 14:24:28 -0600

    install hadoop v2 on ci

--- .ci/install/hadoop.sh
+++ .ci/install/hadoop.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-HADOOP_MIRROR=http://kevinlin.web.rice.edu/static/hadoop-3.0.0-beta1-2.tar.gz
+HADOOP_MIRROR=http://kevinlin.web.rice.edu/static/hadoop-2.8.1.tar.gz
 
 cd $HOME
 if [ ! -d /tmp/cache/hadoop ]; then
     wget --quiet -O hadoop.tar.gz $HADOOP_MIRROR
     tar -xf hadoop.tar.gz -C /tmp
-    mv /tmp/hadoop-3.0.0-beta1 /tmp/cache/hadoop
+    mv /tmp/hadoop-2.8.1 /tmp/cache/hadoop
 fi
 ln -s /tmp/cache/hadoop hadoop
 cp $TRAVIS_BUILD_DIR/config/hdfs-site.xml hadoop/etc/hadoop/hdfs-site.xml
