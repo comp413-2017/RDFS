429eba4ebaa8fe4c9132623c711ea4bf1946c82d
Victoria Nazari <victorianazari@gmail.com>
2017-12-09 11:45:55 -0600

    Change append to use client for now

--- web-rdfs/source/http_handlers.cc
+++ web-rdfs/source/http_handlers.cc
@@ -56,6 +56,14 @@ std::string get_path(std::shared_ptr<HttpsServer::Request> request) {
   return path;
 }
 
+std::string get_file_content(std::string raw_content) {
+  std::string start = "\"file\"";
+  std::string end = "------WebKitFormBoundary";
+  int idxOfStart = raw_content.rfind(start) + start.size();
+  int idxOfEnd = raw_content.substr(idxOfStart).rfind(end);
+  return raw_content.substr(idxOfStart, idxOfEnd);
+}
+
 std::map<std::string, std::string> parseQueryString(std::shared_ptr
                                                     <HttpsServer::Request>
                                                     request) {
@@ -134,26 +142,25 @@ void ls_handler(std::shared_ptr<HttpsServer::Request> request,
 
 void append_file_handler(std::shared_ptr<HttpsServer::Request> request,
                          std::shared_ptr<HttpsServer::Response> response,
+                         std::string content, 
                          std::map<std::string, std::string> requestInfo) {
   LOG(DEBUG) << "HTTP request: append_file_handler";
 
-  hadoop::hdfs::AppendResponseProto res;
-  hadoop::hdfs::AppendRequestProto req;
-
   std::string path = requestInfo["path"];
-  req.set_src(path);
+  LOG(DEBUG) << content;
+  std::string parsedContent = get_file_content(content);
+  LOG(DEBUG) << parsedContent;
 
-  // TODO(Victoria) change so sends correct data
+  std::string tempFile = "tempAppend" + path.substr(1);
+  std::string copyFileReq = "echo " + content + " > " + tempFile;
+  std::string removeFileReq = "rm " + tempFile;
+  std::string input = "hdfs dfs -fs hdfs://localhost:5351 -appendToFile " +
+                      tempFile + " " + path;
 
-//  bool isSuccess = zk->append_file(req, res);
-
-  bool isSuccess = true;
-
-  if (isSuccess) {
-    response->write(SimpleWeb::StatusCode::success_ok);
-  } else {
-    response->write(SimpleWeb::StatusCode::server_error_internal_server_error);
-  }
+  system(copyFileReq.c_str());
+  system(input.c_str());
+  system(removeFileReq.c_str());  // Clean up temp file
+  response->write(SimpleWeb::StatusCode::success_ok);
 }
 
 void set_permission_handler(std::shared_ptr<HttpsServer::Request> request,
@@ -283,7 +290,8 @@ void post_handler(std::shared_ptr<HttpsServer::Response> response,
   std::string typeOfRequest = requestInfo["op"];
 
   if (!typeOfRequest.compare("APPEND")) {
-    append_file_handler(request, response, requestInfo);
+    std::string content = request->content.string();
+    append_file_handler(request, response, content, requestInfo);
   } else {
     bad_request_handler(request, response);
   }
